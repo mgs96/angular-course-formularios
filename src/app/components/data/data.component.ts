5edaaa377ae045a47578db1957448c7ef272e9fe
Mauricio <mauricio.guzman.salazar@gmail.com>
2018-08-02 17:10:03 -0500

    temp

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styles: []
 })
-export class DataComponent implements OnInit {
+export class DataComponent {
 
-  constructor() { }
+  forma: FormGroup;
+  usuario: Object = {
+    nombreCompleto: {
+      nombre: 'Mauricio',
+      apellido: 'Guzman'
+    },
+    correo: 'mauricio@guzman.com'
+  };
 
-  ngOnInit() {
-  }
+  constructor() {
+    this.forma = new FormGroup({
+      'nombreCompleto': new FormGroup({
+        'nombre': new FormControl('', [
+                                        Validators.required,
+                                        Validators.minLength(3)
+                                      ]),
+        'apellido': new FormControl('', Validators.required),
+      }),
+      'correo': new FormControl('', [
+                                      Validators.required,
+                                      Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
+                                      ])
+    });
+   }
+
+   guardarCambios() {
+     console.log(this.usuario);
+     console.log(this.forma.value);
+     console.log(this.forma);
+   }
 
 }
